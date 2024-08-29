03a418ef2efea9e0cfcaba073da2ee29df09ebaa
mofrim <mofrim@posteo.de>
2024-08-29 08:44:51 +0200

    edit comment get_targets

--- smart_targets.c
+++ smart_targets.c
@@ -6,7 +6,7 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 15:19:03 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/08/28 07:12:29 by fmaurer          ###   ########.fr       */
+/*   Updated: 2024/08/29 08:44:11 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,15 +41,18 @@ void	set_targets(t_stacks **sts)
  *
  * The Logic Unravelled: let a_i, a_i+1 in A ...
  *
- * - If elem is the new max of A and old max is not located at the highest index
- *   ATM -> put elem after (max_indx+1) old max.
- * - If a_i+1 > elem > a_i obviously -> elem fits right in between a_i and a_i+1
- *   above a_i+1
- * - If a_i+1 > elem but a_i+1 < a_i, so elem < a_i+1 < a_i. elem has to be
- *   above a_i+1 but the list is rotated in a way that min and max element
- *   meet -> still put it between a_i+1 and a_i
- * - If elem would be the new min of A and the list rotated is rotated in a way
- *   that old min is on top -> put it on top of the list (index 0).
+ * 1) If elem is the new max of A and old max is not located at the highest 
+ *    index ATM -> put elem after (max_indx+1) old max.
+ * 2) If a_i+1 > elem > a_i, obviously -> elem fits right in between a_i and
+ *    a_i+1 above a_i+1
+ * 3) If a_i+1 > elem but a_i+1 < a_i, so elem < a_i+1 < a_i. elem has to be
+ *    above a_i+1 but the list is rotated in a way that min and max element
+ *    meet -> still put it between a_i+1 and a_i
+ * 4) If elem would be the new min of A and the list rotated is rotated in a way
+ *    that old min is on top, or elem is new max and old max is at the bottom of
+ *    the list -> put it on top of the list (index 0).
+ *
+ * That's it.
  */
 int	get_target(long elem, t_stack *s, int size)
 {
