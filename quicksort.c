be2134b24c89984efa6d369658a887efb1283d8e
mofrim <mofrim@posteo.de>
2024-08-29 13:28:54 +0200

    arghls! quicksort_med was faulty, replace

---
+++ quicksort.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   quicksort.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/29 13:18:40 by fmaurer           #+#    #+#             */
+/*   Updated: 2024/08/29 13:20:47 by fmaurer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static int	partition(long **arr, int left, int right);
+static void	swap(long *a, long *b);
+
+static void	quicksort(long **arr, int left, int right)
+{
+	int	part_indx;
+
+	if (left < right)
+	{
+		part_indx = partition(arr, left, right);
+		quicksort(arr, left, part_indx - 1);
+		quicksort(arr, part_indx + 1, right);
+	}
+}
+
+/* Kind of my own algorithm. Neither Lomuto, nor Hoare. But it can deal with
+ * duplicates \o/ */
+static int	partition(long **arr, int left, int right)
+{
+	int		i;
+	int		j;
+	long	pivot;
+
+	i = left;
+	pivot = (*arr)[right];
+	j = right - 1;
+	while (i < j)
+	{
+		while (i < right && (*arr)[i] <= pivot)
+			i++;
+		while (j > i && (*arr)[j] > pivot)
+			j--;
+		if (i < j)
+			swap(&(*arr)[i], &(*arr)[j]);
+	}
+	if ((*arr)[i] > pivot)
+		swap(&(*arr)[i], &(*arr)[right]);
+	else
+		i = right;
+	return (i);
+}
+
+static void	swap(long *a, long *b)
+{
+	long	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+long	*quicksorted_stack(long *arr, int size)
+{
+	long	*sorted_stack;
+	int		i;
+
+	sorted_stack = malloc(size * sizeof(long));
+	if (!sorted_stack)
+		return (NULL);
+	i = -1;
+	while (++i < size)
+		sorted_stack[i] = arr[i];
+	quicksort(&sorted_stack, 0, size - 1);
+	return (sorted_stack);
+}
