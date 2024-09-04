36bd0be2e0c02683d05121848af2235c1740fc6f
Friedemann Maurer <fmaurer@c2r1s2.42wolfsburg.de>
2024-09-04 15:09:02 +0200

    cleanup for eval

--- testing/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(int ac, char **av)
-{
-	for (int i = 0;i < ac;i++) {
-		printf("av[%d] = \"%s\"\n", i, av[i]);
-	}
-	return (0);
-}
--- testing/my_quicksort/quick.h
+++
@@ -1,5 +0,0 @@
-void	quicksort(long **arr, int left, int right);
-void	quicksort_median(long **arr, int left, int right);
-void	quicksort_hoare(long **arr, int left, int right);
-void	quicksort_lomuto(long **arr, int left, int right);
-void	print_arr(long *arr, int l, int r);
